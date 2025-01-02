1b8c6de7b5d4c6954f5f0291add93ff90c5f1af3
gongorinhoo <gongor321@gmail.com>
2025-01-02 19:46:21 +0100

    Projekt

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace HotelReservationApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
